f5c65ac37303772aa39ad3512245c9e49e54105a
Robusr <Robusr@outlook.com>
2026-04-27 02:46:47 +0800

    coding

---
+++ api/urls.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+@File    : urls.py
+@Author  : Robusr
+@Date    : 2026/4/27 02:44
+@Description: api应用路由配置
+@Software: PyCharm
+"""
---
+++ api/views.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+@File    : views.py
+@Author  : Robusr
+@Date    : 2026/4/27 02:45
+@Description: api视图配置
+@Software: PyCharm
+"""
